53f42915f5f9dcf89db740044fecdddec3ce442a
Seth Etter <mail@sethetter.com>
2019-11-29 21:32:16 -0600

    rust, nth-prime: Back to simple functions

--- rust/nth-prime/src/lib.rs
+++ rust/nth-prime/src/lib.rs
@@ -1,39 +1,18 @@
-struct Prime {
-    val: u32,
-}
-
-impl Iterator for Prime {
-    type Item = u32;
-
-    fn next(&mut self) -> Option<u32> {
-        if self.val < 2 {
-            self.val = 2;
-            return Some(2);
-        }
-
-        let n = self.val + 1;
-        for i in n..n*2 {
-            if is_prime(i) {
-                self.val = i;
-                return Some(i);
-            }
-        }
-        None
-    }
-}
-
 pub fn nth(n: u32) -> u32 {
-    let mut p = Prime{val: 0};
-    match p.nth(n as usize) {
-        None => 0,
-        Some(x) => x,
+    let mut prime = 2;
+    for _ in 0..n {
+        prime = next_prime(prime);
     }
+    prime
+}
+
+pub fn next_prime(mut x: u32) -> u32 {
+    while !is_prime(x+1) { x += 1; }
+    x+1
 }
 
 pub fn is_prime(x: u32) -> bool {
     if x == 0 || x == 1 { return false; }
-    for i in 2..x-1 {
-        if x != i && x % i == 0 { return false; }
-    }
+    for i in 2..(x/2)+1 { if x % i == 0 { return false; } }
     true
 }
